dc54467e9f5a67b686d9bb86aa565a11e7be53cc
mvchfvn <mimi123meg@gmail.com>
2018-07-24 20:43:00 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,10 +9,12 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  return STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  
 end
 
 def last_student_by_index
